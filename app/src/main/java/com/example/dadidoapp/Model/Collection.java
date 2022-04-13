eead06e8f3bf1192efccb5fe67d5850ecc68019d
BUDIARIYANSA <672019076@student.uksw.edu>
2022-04-13 22:07:00 +0700

    update model

--- app/src/main/java/com/example/dadidoapp/Model/Collection.java
+++ app/src/main/java/com/example/dadidoapp/Model/Collection.java
@@ -1,4 +1,57 @@
 package com.example.dadidoapp.Model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Collection {
+
+    @SerializedName("collection_name")
+    private String collectionName;
+
+    @SerializedName("url")
+    private String url;
+
+    @SerializedName("description")
+    private String description;
+
+    @SerializedName("pathfile_server")
+    private String pathFileServer;
+
+    public Collection(String collectionName, String url, String description, String pathFileServer) {
+        this.collectionName = collectionName;
+        this.url = url;
+        this.description = description;
+        this.pathFileServer = pathFileServer;
+    }
+
+    public String getCollectionName() {
+        return collectionName;
+    }
+
+    public void setCollectionName(String collectionName) {
+        this.collectionName = collectionName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPathFileServer() {
+        return pathFileServer;
+    }
+
+    public void setPathFileServer(String pathFileServer) {
+        this.pathFileServer = pathFileServer;
+    }
 }
--- app/src/main/java/com/example/dadidoapp/Model/Transaction.java
+++ app/src/main/java/com/example/dadidoapp/Model/Transaction.java
@@ -4,5 +4,54 @@ import com.google.gson.annotations.SerializedName;
 
 public class Transaction {
 
-    @SerializedName()
+    @SerializedName("id_item")
+    private Integer idItem;
+
+    @SerializedName("id_profile_buyer")
+    private Integer idProfileBuyer;
+
+    @SerializedName("id_profile_Seller")
+    private Integer idProfileSeller;
+
+    @SerializedName("transaction_time")
+    private String transactionTime;
+
+    public Transaction(Integer idItem, Integer idProfileBuyer, Integer idProfileSeller, String transactionTime) {
+        this.idItem = idItem;
+        this.idProfileBuyer = idProfileBuyer;
+        this.idProfileSeller = Transaction.this.idProfileSeller;
+        this.transactionTime = Transaction.this.transactionTime;
+    }
+
+    public Integer getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(Integer idItem) {
+        this.idItem = idItem;
+    }
+
+    public Integer getIdProfileBuyer() {
+        return idProfileBuyer;
+    }
+
+    public void setIdProfileBuyer(Integer idProfileBuyer) {
+        this.idProfileBuyer = idProfileBuyer;
+    }
+
+    public Integer getIdProfileSeller() {
+        return idProfileSeller;
+    }
+
+    public void setIdProfileSeller(Integer idProfileSeller) {
+        this.idProfileSeller = idProfileSeller;
+    }
+
+    public String getTransactionTime() {
+        return transactionTime;
+    }
+
+    public void setTransactionTime(String transactionTime) {
+        this.transactionTime = transactionTime;
+    }
 }
