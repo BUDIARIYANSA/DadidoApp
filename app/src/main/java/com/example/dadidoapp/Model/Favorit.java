6262a8511dc3714cf97476658092e70861882f5a
BUDIARIYANSA <672019076@student.uksw.edu>
2022-04-13 21:27:31 +0700

    menambahkan beberapa model

---
+++ app/src/main/java/com/example/dadidoapp/Model/Favorit.java
@@ -0,0 +1,4 @@
+package com.example.dadidoapp.Model;
+
+public class Favorit {
+}
---
+++ app/src/main/java/com/example/dadidoapp/Model/Transaction.java
@@ -0,0 +1,8 @@
+package com.example.dadidoapp.Model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Transaction {
+
+    @SerializedName()
+}
