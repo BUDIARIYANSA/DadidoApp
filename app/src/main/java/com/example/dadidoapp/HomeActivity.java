9f5db0cf8b7cda1d5ed1fab799a141110f1c3633
dece441 <danieldc01@gmail.com>
2022-04-18 00:21:01 +0700

    Fixing action Bar, adding LogOut and remember me in Login

--- app/src/main/java/com/example/dadidoapp/HomeActivity.java
+++ app/src/main/java/com/example/dadidoapp/HomeActivity.java
@@ -3,13 +3,15 @@ package com.example.dadidoapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -22,6 +24,38 @@ public class HomeActivity extends AppCompatActivity {
     Button new_btn;
     Button trending_btn;
     FloatingActionButton mCollection;
+    Toolbar toolbar;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.mProfile:
+                Intent intent = new Intent(HomeActivity.this, ProfilActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.mLogout:
+                sharedPreferences=getSharedPreferences("LoginPrefs", MODE_PRIVATE);
+                editor=sharedPreferences.edit();
+                editor.putString("username","");
+                editor.putString("password","");
+                editor.commit();
+                Intent intent2 = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,7 +110,7 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.mHome: startActivity(new Intent(HomeActivity.this, HomeActivity.class)); finish();
+                    case R.id.mHome: //startActivity(new Intent(HomeActivity.this, HomeActivity.class)); finish();
                         break;
                     case R.id.mFavorite: startActivity(new Intent(HomeActivity.this, FavoritActivity.class)); finish();
                         break;
@@ -84,5 +118,8 @@ public class HomeActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+
+
     }
 }
