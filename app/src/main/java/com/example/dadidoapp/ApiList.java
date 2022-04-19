c57de36fed1e05cac7f38dd3c7ceb47dc94c5532
BUDIARIYANSA <672019076@student.uksw.edu>
2022-04-19 10:51:14 +0700

    fixing profile dan update profile

--- app/src/main/java/com/example/dadidoapp/ApiList.java
+++ app/src/main/java/com/example/dadidoapp/ApiList.java
@@ -1,6 +1,8 @@
 package com.example.dadidoapp;
 import com.example.dadidoapp.Model.Creator;
 import com.example.dadidoapp.Model.Item;
+import com.example.dadidoapp.Model.Profile;
+import com.example.dadidoapp.Model.User;
 
 import java.util.ArrayList;
 
@@ -26,4 +28,10 @@ public interface ApiList {
 
     @POST("webservice.php")
     Call<ArrayList<Creator>> cardCreator(@Body RequestBody body);
+
+    @POST("webservice.php")
+    Call<ArrayList<Profile>> profile(@Body RequestBody body);
+
+    @POST("webservice.php")
+    Call<Object> updateProfile(@Body RequestBody body);
 }
--- app/src/main/java/com/example/dadidoapp/ProfilActivity.java
+++ app/src/main/java/com/example/dadidoapp/ProfilActivity.java
@@ -1,18 +1,34 @@
 package com.example.dadidoapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.dadidoapp.Model.Profile;
+import com.example.dadidoapp.Model.User;
 import com.google.android.material.textfield.TextInputLayout;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ProfilActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
@@ -22,12 +38,14 @@ public class ProfilActivity extends AppCompatActivity {
 
     private ImageView profile_url;
     private Button update;
-    private EditText username;
-    private EditText home_address;
-    private EditText fullname;
-    private EditText oldpass;
-    private EditText newpass;
-    private EditText confirm_newpass;
+    private TextInputLayout username;
+    private TextInputLayout email;
+    private TextInputLayout home_address;
+    private TextInputLayout fullname;
+    private TextInputLayout oldpass;
+    private TextInputLayout newpass;
+    private TextInputLayout confirm_newpass;
+    private ApiList apiList = RetrofitClient.getRetrofitClient().create(ApiList.class);
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) { //Showing back button
@@ -53,14 +71,110 @@ public class ProfilActivity extends AppCompatActivity {
         // showing the back button in action bar
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        profile_url = (ImageView) findViewById(R.id.imageProfile);
+        username = (TextInputLayout) findViewById(R.id.textInputLayoutUsername);
+        email = (TextInputLayout) findViewById(R.id.textInputLayoutEmail);
+        fullname = (TextInputLayout) findViewById(R.id.textInputLayoutFullmame);
+        home_address = (TextInputLayout) findViewById(R.id.textInputLayoutHomeAddress);
+        oldpass = (TextInputLayout) findViewById(R.id.textInputLayoutOldPassword);
+        newpass = (TextInputLayout) findViewById(R.id.textInputLayoutNewPassword);
+        confirm_newpass = (TextInputLayout) findViewById(R.id.textInputLayoutConfirmNewPassword);
+
+        loadDataProfile();
+
+        update = (Button) findViewById(R.id.buttonUpdate);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateDataProfile();
+            }
+        });
+    }
+
+    public void updateDataProfile() {
+
+        String user_name = username.getEditText().getText().toString().trim();
+        String user_fullname = fullname.getEditText().getText().toString().trim();
+        String user_email = email.getEditText().getText().toString().trim();
+        String user_home_address = home_address.getEditText().getText().toString().trim();
+        String old_pass = oldpass.getEditText().getText().toString().trim();
+        String new_pass = newpass.getEditText().getText().toString().trim();
+        String confirm_newPass = confirm_newpass.getEditText().getText().toString().trim();
+
+        if(!(new_pass).equals(confirm_newPass)) {
+            Toast.makeText(ProfilActivity.this, "New password and confirm new password not same!", Toast.LENGTH_SHORT).show();
+        } else {
+            RequestBody requestBody = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("CMD", "update_profile")
+                    .addFormDataPart("username", user_name)
+                    .addFormDataPart("email", user_email)
+                    .addFormDataPart("fullname", user_fullname)
+                    .addFormDataPart("home_address", user_home_address)
+                    .addFormDataPart("old_password", old_pass)
+                    .addFormDataPart("new_password", new_pass)
+                    .build();
+
+            Call call = apiList.updateProfile(requestBody);
+            call.enqueue(new Callback() {
+                @Override
+                public void onResponse(Call call, Response response) {
+                    if(response.isSuccessful()) {
+                        String res = response.body().toString();
+                        if(res.equals("success")) {
+                            setPreference(ProfilActivity.this, "username", user_name);
+                            setPreference(ProfilActivity.this, "password", new_pass);
+
+                            Toast.makeText(ProfilActivity.this, "Update profile successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(ProfilActivity.this, HomeActivity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(ProfilActivity.this, "Update profile failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(Call call, Throwable t) {
+
+                }
+            });
+        }
+
+
+    }
+
+    public void loadDataProfile() {
         String str_username = getPreference(ProfilActivity.this, "username");
         String str_password = getPreference(ProfilActivity.this, "password");
 
-        username = (EditText) findViewById(R.id.textInputEditText_username);
-        oldpass = (EditText) findViewById(R.id.textInputEditText_OldPassword);
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("CMD", "profile_user")
+                .addFormDataPart("username", str_username)
+                .addFormDataPart("password", str_password)
+                .build();
+        Call<ArrayList<Profile>> call = apiList.profile(requestBody);
+        call.enqueue(new Callback<ArrayList<Profile>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Profile>> call, Response<ArrayList<Profile>> response) {
+                if(response.isSuccessful()) {
+                    ArrayList<Profile> data = response.body();
 
-        username.setText(str_username);
-        oldpass.setText(str_password);
+                    Picasso.get().load(data.get(0).getProfileURL()).into(profile_url);
+                    username.getEditText().setText(data.get(0).getUsername());
+                    fullname.getEditText().setText(data.get(0).getFullname());
+                    email.getEditText().setText(data.get(0).getEmail());
+                    oldpass.getEditText().setText(data.get(0).getPassword());
+                    home_address.getEditText().setText(data.get(0).getHomeAddress());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Profile>> call, Throwable t) {
+
+            }
+        });
     }
 
     public static boolean setPreference(Context context, String key, String value) {
