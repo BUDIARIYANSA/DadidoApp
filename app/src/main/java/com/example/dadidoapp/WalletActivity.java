a29543c33f2c816ed5ba3947e95eb3b9432ac3c2
BUDIARIYANSA <672019076@student.uksw.edu>
2022-04-21 13:10:18 +0700

    menghubungkan fungsi wallet

--- app/src/main/java/com/example/dadidoapp/WalletActivity.java
+++ app/src/main/java/com/example/dadidoapp/WalletActivity.java
@@ -1,18 +1,43 @@
 package com.example.dadidoapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.dadidoapp.Model.Wallet;
+
+import org.w3c.dom.Text;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class WalletActivity extends AppCompatActivity {
     private Button buttonAddFunds;
+    private TextView totalFunds;
+    private ApiList apiList = RetrofitClient.getRetrofitClient().create(ApiList.class);
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private static final String PREFS_NAME = "LoginPrefs";
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) { //Showing back button
@@ -38,6 +63,10 @@ public class WalletActivity extends AppCompatActivity {
         // showing the back button in action bar
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        totalFunds = (TextView) findViewById(R.id.totalFunds);
+
+        getFunds();
+
         buttonAddFunds = (Button) findViewById(R.id.buttonAddFunds);
         buttonAddFunds.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +74,39 @@ public class WalletActivity extends AppCompatActivity {
                 startActivity(new Intent(WalletActivity.this, WalletTwoActivity.class));
             }
         });
+    }
+
+    public void getFunds() {
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("CMD", "get_funds")
+                .addFormDataPart("username", getPreference(getApplicationContext(), "username"))
+                .addFormDataPart("password", getPreference(getApplicationContext(), "password"))
+                .build();
+        Call<ArrayList<Wallet>> call = apiList.getWallet(requestBody);
+        call.enqueue(new Callback<ArrayList<Wallet>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Wallet>> call, Response<ArrayList<Wallet>> response) {
+                if (response.isSuccessful()) {
+                    ArrayList<Wallet> data = response.body();
+                    DecimalFormat df = new DecimalFormat("0", DecimalFormatSymbols.getInstance(Locale.ENGLISH));
+                    df.setMaximumFractionDigits(2);
+                    totalFunds.setText(df.format(Float.parseFloat(data.get(0).getTotalFund())) + " " + data.get(0).getWalletType().toUpperCase(Locale.ROOT));
+                } else {
+                    Toast.makeText(WalletActivity.this, "Failed to load data..!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Wallet>> call, Throwable t) {
+                Toast.makeText(WalletActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public static String getPreference(Context context, String key) {
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        return settings.getString(key, "");
     }
 }
--- app/src/main/java/com/example/dadidoapp/WalletTwoActivity.java
+++ app/src/main/java/com/example/dadidoapp/WalletTwoActivity.java
@@ -1,14 +1,34 @@
 package com.example.dadidoapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class WalletTwoActivity extends AppCompatActivity {
+    private Button buttonDeposit;
+    private TextInputLayout totalFunds;
+    private ApiList apiList = RetrofitClient.getRetrofitClient().create(ApiList.class);
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private static final String PREFS_NAME = "LoginPrefs";
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) { //Showing back button
@@ -33,5 +53,55 @@ public class WalletTwoActivity extends AppCompatActivity {
 
         // showing the back button in action bar
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        totalFunds = (TextInputLayout) findViewById(R.id.textInputLayoutMoney);
+        buttonDeposit = (Button) findViewById(R.id.buttonDeposit);
+        buttonDeposit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RequestBody requestBody = new MultipartBody.Builder()
+                        .setType(MultipartBody.FORM)
+                        .addFormDataPart("CMD", "add_funds")
+                        .addFormDataPart("username", getPreference(getApplicationContext(), "username"))
+                        .addFormDataPart("password", getPreference(getApplicationContext(), "password"))
+                        .addFormDataPart("total_funds", totalFunds.getEditText().getText().toString().trim())
+                        .build();
+                Call call = apiList.deposit(requestBody);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onResponse(Call call, Response response) {
+                        if (response.isSuccessful()) {
+                            String res = response.body().toString();
+                            if(res.equals("success")) {
+                                startActivity(new Intent(WalletTwoActivity.this, HomeActivity.class));
+                                finish();
+                                Toast.makeText(WalletTwoActivity.this, "Deposit funds Successful!!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(WalletTwoActivity.this, "Deposit funds failed!!", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(WalletTwoActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call call, Throwable t) {
+                        Toast.makeText(WalletTwoActivity.this, "Error : "+t, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    public static boolean setPreference(Context context, String key, String value) {
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(key, value);
+        return editor.commit();
+    }
+
+    public static String getPreference(Context context, String key) {
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        return settings.getString(key, "");
     }
 }
