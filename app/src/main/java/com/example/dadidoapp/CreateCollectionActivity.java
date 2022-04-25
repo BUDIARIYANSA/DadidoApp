2db782094ee0f2ad96ff20e4a7e8288fab1866bd
LeonardoAde <672019142@student.uksw.edu>
2022-04-25 21:25:17 +0700

    create collection activity update 1

--- app/src/main/java/com/example/dadidoapp/CreateCollectionActivity.java
+++ app/src/main/java/com/example/dadidoapp/CreateCollectionActivity.java
@@ -3,20 +3,34 @@ package com.example.dadidoapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class CreateCollectionActivity extends AppCompatActivity {
 
     Button Uploud_Btn;
     ImageView img;
+    private TextInputLayout c_name;
+    private TextInputLayout c_desc;
 
     int SELECT_PICTURE = 200;
+
+    private String path;
+
+    private static final String PREFS_NAME = "LoginPrefs";
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) { //Showing back button
@@ -44,6 +58,8 @@ public class CreateCollectionActivity extends AppCompatActivity {
         actionBar.setTitle("Create Your New Collection");
 
         Uploud_Btn=(Button)findViewById(R.id.SelectImageBtn);
+        c_name = (TextInputLayout) findViewById(R.id.textInputLayoutUsername);
+        c_desc = (TextInputLayout) findViewById(R.id.textInputLayoutdesc);
         img=(ImageView)findViewById(R.id.img);
 
         Uploud_Btn.setOnClickListener(new View.OnClickListener() {
@@ -57,11 +73,10 @@ public class CreateCollectionActivity extends AppCompatActivity {
 
     private void imageChooser() {
 
-        Intent i = new Intent();
-        i.setType("image/*");
-        i.setAction(Intent.ACTION_GET_CONTENT);
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
 
-        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+        startActivityForResult(Intent.createChooser(pickIntent, "Select Picture"), SELECT_PICTURE);
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -75,8 +90,33 @@ public class CreateCollectionActivity extends AppCompatActivity {
                 if (null != selectImageUri) {
 
                     img.setImageURI(selectImageUri);
+                    path = getRealPathFromURI(selectImageUri,CreateCollectionActivity.this);
+                    Toast.makeText(CreateCollectionActivity.this, path, Toast.LENGTH_SHORT).show();
                 }
             }
         }
+    }
+
+    public String getRealPathFromURI(Uri contentUri, Context context) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } catch (Exception e) {
+            //Log.e("EditActivity", "getRealPathFromURI Exception : " + e.toString());
+            return "";
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    public static String getPreference(Context context, String key) {
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        return settings.getString(key, "");
     }
 }
